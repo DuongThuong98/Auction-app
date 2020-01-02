569e5d51e2f1f2b1a415335bfedd741a215e14fb
= <=>
2020-01-02 17:36:31 +0700

    DONE Edit san pham

--- routes/seller/product.route.js
+++ routes/seller/product.route.js
@@ -4,7 +4,7 @@ const moment = require('moment');
 const multer = require('multer');
 const userModel = require('../../models/user.model');
 const productModel = require('../../models/product.model');
-const categoryModel = require('../../models/product.model');
+const categoryModel = require('../../models/category.model');
 const subImageModel = require('../../models/subImage.model');
 const wishlistModel = require('../../models/wishlist.model');
 const autionHistoryModel = require('../../models/auctionHistory.model');
@@ -82,8 +82,10 @@ router.post('/add', upload.array('fuMain', 4), async (req, res) => {
     if (result.affectedRows == 1) {
         if (req.files.length > 1) {
             for (i = 1; i < req.files.length; i++) {
-                const entityImage = { id_product: result.insertId,
-                                      image: req.files[i].filename };
+                const entityImage = {
+                    id_product: result.insertId,
+                    image: req.files[i].filename
+                };
                 subImageModel.add(entityImage);
             }
         }
@@ -91,6 +93,42 @@ router.post('/add', upload.array('fuMain', 4), async (req, res) => {
     res.render('vwSeller/addPro');
 })
 
+router.get('/edit/:id', async (req, res) => {
+    const rows = await productModel.single(req.params.id);
+
+    if (rows.length === 0) {
+        throw new Error('Invalid product id');
+    }
+    const type = await categoryModel.single(rows[0].id_type);
+    //console.log(rows[0]);
+    //console.log(type);
+
+    res.render('vwSeller/editPro', { product: rows[0], typeName: type[0].cate_name });
+})
+
+
+
+router.post('/patch', async (req, res) => {
+    const rows = await productModel.single(req.body.id);
+
+    if (rows.length === 0) {
+        throw new Error('Invalid product id');
+    }
+    const type = await categoryModel.single(rows[0].id_type);
+    //console.log(rows[0]);
+    //console.log(type);
+
+    current_time = moment().format('LLLL'); 
+    rows[0].detail = "</br>" + rows[0].detail + '<b>'+current_time+'</b> </br>' +req.body.moreDetail ;
+
+    console.log(req.body);
+    console.log(rows[0].detail);
+    entity = {detail: rows[0].detail,
+                ProID: req.body.id};
+    productModel.patch(entity);
+    //res.render('vwSeller/editPro', { product: rows[0], typeName: type[0].cate_name });
+    res.redirect('/seller/product');
+})
 
 
 module.exports = router;
